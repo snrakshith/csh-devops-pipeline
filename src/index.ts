29434dda06fa1a436816829bb4b9d7a43e4093bd
snrakshith <snrakshith.97@gmail.com>
2023-12-01 12:23:19 +0530

    refactor: change in csh architecture for devops

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-
-const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({ status: 200, message: "Fine!!!" });
-});
-
-app.listen("4000", () => console.log("listening on 4000"));
